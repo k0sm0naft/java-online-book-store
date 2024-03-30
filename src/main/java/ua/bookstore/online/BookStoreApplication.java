89f046d6d998ff5f372f43c565d7dec16e4a18e9
Artem Akymenko <akimenko.artem@gmail.com>
2024-03-30 16:56:36 +0200

    Initial commit

---
+++ src/main/java/ua/bookstore/online/BookStoreApplication.java
@@ -0,0 +1,13 @@
+package ua.bookstore.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/bookstore/online/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.bookstore.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
